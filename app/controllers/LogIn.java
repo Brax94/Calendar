1914f903d6d8f7fd3bff6a25ee30fb10a16a2bc9
EliasBHagen <Elias.B.Hagen@Gmail.com>
2015-02-25 10:58:56 +0100

    Users can now register

--- app/controllers/LogIn.java
+++ app/controllers/LogIn.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import models.Bruker;
+import play.data.Form;
 import play.mvc.Result;
 import views.html.layoutHtml;
 
+import static play.data.Form.form;
 import static play.mvc.Results.ok;
 
 /**
@@ -17,5 +20,12 @@ public class LogIn {
     public static Result signUp(){
         return ok(layoutHtml.render("SignUp", views.html.Login.signUp.render()));
     }
+
+    public static void registerUser(){
+        Form<Bruker> userForm = form(Bruker.class).bindFromRequest();
+        Bruker userModel = userForm.get();
+        userModel.save();
+    }
+
 
 }
