1669c6fd548013f17370b7826616d5ca31366433
EliasBHagen <Elias.B.Hagen@Gmail.com>
2015-03-11 14:28:55 +0100

    2.sql now works

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,3 +1,6 @@
+# --- !Ups
 INSERT INTO PUBLIC.BRUKER(USERNAME, PASSWORD, EMAIL, FIRST_NAME, LAST_NAME) VALUES('eliasbh', 'password', 'elias.b.hagen@gmail.com', 'Elias Bragstad', 'Hagen');
 INSERT INTO PUBLIC.BRUKER(USERNAME, PASSWORD, EMAIL, FIRST_NAME, LAST_NAME) VALUES('carlaj', 'password', 'julsvoll@gmail.com', 'Carl Andreas', 'Julsvoll');
 INSERT INTO PUBLIC.BRUKER(USERNAME, PASSWORD, EMAIL, FIRST_NAME, LAST_NAME) VALUES('valdemer', 'password', 'rolfsen@gmail.com', 'Valdemar', 'Rolfsen');
+
+# --- !Downs
