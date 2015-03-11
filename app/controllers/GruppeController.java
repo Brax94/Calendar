7825e09cfc59a8d24cd47c865fcd4b7a8edf6de1
EliasBHagen <Elias.B.Hagen@Gmail.com>
2015-03-11 15:29:22 +0100

    Grupper ready for work

---
+++ app/controllers/GruppeController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by eliasbragstadhagen on 11.03.15.
+ */
+public class GruppeController extends Controller{
+
+    public Result index(){
+        return ok();
+    }
+
+}
